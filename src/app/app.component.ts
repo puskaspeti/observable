909bd4269d440eeef259e9d53db9b81a862270d7
Puskás Péter <puskas.peter@autsoft.hu>
2018-06-01 10:39:52 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, timer, concat, fromEvent, interval, Observable, Subscription, forkJoin} from 'rxjs';
+import {map, mergeMap, mapTo, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  subscriptions: Subscription[] = [];
+
+  reset() {
+    this.subscriptions.map(s => s.unsubscribe());
+  }
+
+  /**
+   * Subscribe to Observables in order but only when the previous completes, let me know, then move to the next one.
+   */
+  concat() {
+    const postOne$ = timer(2000).pipe(mapTo({ id: 1}));
+    const postTwo$ = timer(5000).pipe(mapTo({ id: 2}));
+    const postThree$ = timer(2000).pipe(mapTo({ id: 3}));
+
+    const unsubscribe = concat(postOne$, postTwo$, postThree$).subscribe(val => console.log(val));
+
+    this.subscriptions.push(unsubscribe);
+  }
+
+  /**
+   * Don’t let me know until all the Observables are complete, then give me all the values at once. ( Array )
+   * (forkJoin is the Promise.all() of Rx.)
+   */
+  forkJoin() {
+    const postOne$ = timer(2000).pipe(mapTo({ id: 1}));
+    const postTwo$ = timer(5000).pipe(mapTo({ id: 2}));
+    const postThree$ = timer(2000).pipe(mapTo({ id: 3}));
+
+    const unsubscribe = forkJoin(postOne$, postTwo$, postThree$).subscribe(val => console.log(val));
+
+    this.subscriptions.push(unsubscribe);
+  }
+
+  /**
+   * Only when the inner Observable emits, let me know by merging the value to the outer Observable.
+   */
+  mergeMap() {
+    // Source ( or outer ) Observable
+    const clicks$ = fromEvent(document, 'click');
+    // Inner Observable
+    const innerObservable$ = interval(1000);
+
+    const unsubscribe = clicks$.pipe(mergeMap(event => innerObservable$)).subscribe(val => console.log(val));
+
+    this.subscriptions.push(unsubscribe);
+  }
+
+  /**
+   * Like mergeMap but when the source Observable emits cancel any previous subscriptions of the inner Observable.
+   */
+  switchMap() {
+    // Source ( or outer ) Observable
+    const clicks$ = fromEvent(document, 'click');
+    // Inner Observable
+    const innerObservable$ = interval(1000);
+
+    const unsubscribe = clicks$.pipe(switchMap(event => innerObservable$)).subscribe(val => console.log(val));
+
+    this.subscriptions.push(unsubscribe);
+  }
 }
